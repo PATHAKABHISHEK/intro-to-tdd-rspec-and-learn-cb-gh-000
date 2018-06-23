2160716dfe8b15b7a58b18096ab636222d9509dd
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-06-23 07:29:27 +0000

    automatically backed up by learn

--- spec/multiplication_calculator.rb
+++ spec/multiplication_calculator.rb
@@ -1,3 +1,10 @@
 require_relative ''../multiplicator.rb'
 describe "multiplication method" do
-it "returns multiplication result by carrying out multiplication of two numbers" do
+  it "returns multiplication result by carrying out multiplication of two numbers" do
+
+      answer_multiplication = multiplication(42,3)
+
+      expect(answer_multiplication).to.eq(126)
+
+  end
+end
